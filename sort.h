f6a9d2ee98fea99ecc853fbac4118b2303bc14bf
Amine Meftah <amine.gamer222@gmail.com>
2023-11-20 14:59:26 +0100

    sort.h update

--- sort.h
+++ sort.h
@@ -23,5 +23,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void QS(int *array, size_t start, size_t end, size_t size);
+void swap(int *a, int*b);
 
 #endif /*__Sorting__*/
